28a0bb4d30c2f893e1e52acca976722eeb4c837d
Jasper <jasper.lammers@gmail.com>
2025-04-11 13:47:31 +0200

    New blazor project with NetDaemon reference.

--- CodeCasa/CodeCasa/Program.cs
+++ CodeCasa/CodeCasa/Program.cs
@@ -5,26 +5,24 @@ try
     // This ensures that the log files are written relative the application's launch folder.
     Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-    await Host.CreateDefaultBuilder(args)
-        .UseNetDaemonAppSettings()
-        // .UseNetDaemonDefaultLogging()
-        .UseGingerLogging()
-        .UseNetDaemonRuntime()
-        .UseNetDaemonTextToSpeech()
-        .ConfigureServices((_, services) =>
-            services
-                .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
-                .AddNetDaemonStateManager()
-                .AddNetDaemonScheduler()
-                .AddHomeAssistantGenerated()
-                .AddGinger()
-        )
-        .Build()
-        .RunAsync()
-        .ConfigureAwait(false);
+    //await Host.CreateDefaultBuilder(args)
+    //    .UseNetDaemonAppSettings()
+    //    // .UseNetDaemonDefaultLogging()
+    //    .UseNetDaemonRuntime()
+    //    .UseNetDaemonTextToSpeech()
+    //    .ConfigureServices((_, services) =>
+    //        services
+    //            .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
+    //            .AddNetDaemonStateManager()
+    //            .AddNetDaemonScheduler()
+    //            .AddHomeAssistantGenerated()
+    //    )
+    //    .Build()
+    //    .RunAsync()
+    //    .ConfigureAwait(false);
 }
 catch (Exception e)
 {
-    Log.Logger.Error($"Failed to start host... {e}");
+    //Log.Logger.Error($"Failed to start host... {e}");
     throw;
 }
