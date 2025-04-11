01f04d6fa002116bdc69daf3436c3450d133c732
Jasper <jasper.lammers@gmail.com>
2025-04-11 13:00:20 +0200

    Empty console project

---
+++ CodeCasa/CodeCasa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+try
+{
+    // This ensures that the log files are written relative the application's launch folder.
+    Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+    await Host.CreateDefaultBuilder(args)
+        .UseNetDaemonAppSettings()
+        // .UseNetDaemonDefaultLogging()
+        .UseGingerLogging()
+        .UseNetDaemonRuntime()
+        .UseNetDaemonTextToSpeech()
+        .ConfigureServices((_, services) =>
+            services
+                .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddNetDaemonStateManager()
+                .AddNetDaemonScheduler()
+                .AddHomeAssistantGenerated()
+                .AddGinger()
+        )
+        .Build()
+        .RunAsync()
+        .ConfigureAwait(false);
+}
+catch (Exception e)
+{
+    Log.Logger.Error($"Failed to start host... {e}");
+    throw;
+}
