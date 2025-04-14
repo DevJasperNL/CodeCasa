bd300a10baada706420f65f539789dfbde47e9f6
Jasper <jasper.lammers@gmail.com>
2025-04-14 13:34:50 +0200

    Implemented automations project including serilog configuration.

--- src/CodeCasa.Automations/Program.cs
+++ src/CodeCasa.Automations/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using CodeCasa.Automations.Extensions;
+using Microsoft.Extensions.Hosting;
+using NetDaemon.AppModel;
+using NetDaemon.Runtime;
+using Serilog;
+
+#pragma warning disable CA1812
+
+try
+{
+    // This ensures that the log files are written relative the application's launch folder.
+    Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+    await Host.CreateDefaultBuilder(args)
+        .UseCodeCasa()
+        .UseNetDaemonRuntime()
+        .ConfigureServices((_, services) =>
+            services
+                .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddCodeCasa()
+        )
+        .Build()
+        .RunAsync()
+        .ConfigureAwait(false);
+}
+catch (Exception e)
+{
+    Log.Logger.Error($"Failed to start host... {e}");
+    throw;
+}
