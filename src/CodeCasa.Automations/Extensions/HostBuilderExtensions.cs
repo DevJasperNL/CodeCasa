75e5e549fec1bf9ff45814925d05c7334a940d8a
Jasper Lammers <81427514+DevJasperNL@users.noreply.github.com>
2025-04-14 13:39:38 +0200

    Implemented empty automation project with logging. (#3)
    * Created new console project.
    
    * Adding development app settings.
    
    * Untracking development appsettings
    
    * Implemented automations project including serilog configuration.
    
    * Create appsettings.Development.json
    
    * Update appsettings.Development.json
    
    * Delete src/CodeCasa.Automations/appsettings.Development.json
    
    * Create appsettings.Development.json
    
    * Create appsettings.Development.json

---
+++ src/CodeCasa.Automations/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace CodeCasa.Automations.Extensions
+{
+    internal static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseCodeCasa(this IHostBuilder builder)
+        {
+            return builder.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+        }
+    }
+}
---
+++ src/CodeCasa.Automations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using CodeCasa.Automations.Extensions;
+using Microsoft.Extensions.Hosting;
+using NetDaemon.AppModel;
+using NetDaemon.Runtime;
+using Serilog;
+
+#pragma warning disable CA1812
+
+try
+{
+    // This ensures that the log files are written relative the application's launch folder.
+    Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+    await Host.CreateDefaultBuilder(args)
+        .UseCodeCasa()
+        .UseNetDaemonRuntime()
+        .ConfigureServices((_, services) =>
+            services
+                .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddCodeCasa()
+        )
+        .Build()
+        .RunAsync()
+        .ConfigureAwait(false);
+}
+catch (Exception e)
+{
+    Log.Logger.Error($"Failed to start host... {e}");
+    throw;
+}
