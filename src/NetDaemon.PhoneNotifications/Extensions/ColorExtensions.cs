d60974b6b38539580188d7ccf50721d285f1259b
Jasper <jasper.lammers@gmail.com>
2025-07-25 16:04:10 +0200

    Moved phone notification logic to netdaemon utils.

--- src/NetDaemon.PhoneNotifications/Extensions/ColorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace NetDaemon.PhoneNotifications.Extensions;
-
-internal static class ColorExtensions
-{
-    public static string ToHex(this Color color)
-        => $"#{color.R:X2}{color.G:X2}{color.B:X2}";
-}
--- src/NetDaemon.PhoneNotifications/PhoneNotification.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NetDaemon.PhoneNotifications;
-
-public class PhoneNotification(string id, IDisposable disposable) : IDisposable
-{
-    public string Id { get; } = id;
-    public void Dispose() => disposable.Dispose();
-}
