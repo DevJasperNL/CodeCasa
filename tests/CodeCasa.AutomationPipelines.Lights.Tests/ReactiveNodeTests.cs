2e158c638849d7a218702c88143aff580d45798e
Jasper <jasper.lammers@gmail.com>
2026-01-03 16:04:49 +0100

    Updated unit tests

--- tests/CodeCasa.AutomationPipelines.Lights.Tests/ReactiveNodeTests.cs
+++ tests/CodeCasa.AutomationPipelines.Lights.Tests/ReactiveNodeTests.cs
@@ -1,18 +1,13 @@
 ﻿using CodeCasa.AutomationPipelines.Lights.Nodes;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using CodeCasa.AutomationPipelines.Lights.ReactiveNode;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reactive.Concurrency;
 using CodeCasa.Lights;
 using CodeCasa.AutomationPipelines.Lights.Pipeline;
 using Microsoft.Extensions.Logging;
-using System.Linq;
 using Moq;
 using System.Reactive.Subjects;
 using CodeCasa.AutomationPipelines.Lights.Context;
-using CodeCasa.AutomationPipelines;
 using ReactiveNodeClass = CodeCasa.AutomationPipelines.Lights.ReactiveNode.ReactiveNode;
 
 namespace CodeCasa.AutomationPipelines.Lights.Tests
@@ -274,6 +269,84 @@ namespace CodeCasa.AutomationPipelines.Lights.Tests
             // Assert 2
             _scopeFactoryMock.Verify(x => x.CreateScope(), Times.Exactly(2), "New scope should be created on second trigger");
             _scopeMock.As<IAsyncDisposable>().Verify(x => x.DisposeAsync(), Times.Once, "Old scope should be disposed");
+        }
+
+        [TestMethod]
+        public void TurnOffWhen_Triggered_EmitsOffAndPassesThrough()
+        {
+            // Arrange
+            var triggerSubject = new Subject<int>();
+            
+            // Act
+            var node = _reactiveNodeFactory.CreateReactiveNode(_lightMock.Object, config => 
+            {
+                config.TurnOffWhen(triggerSubject);
+            });
+
+            LightTransition? lastOutput = null;
+            node.OnNewOutput.Subscribe(output => lastOutput = output);
+
+            // Trigger
+            triggerSubject.OnNext(1);
+
+            // Assert
+            Assert.IsNotNull(lastOutput);
+            Assert.AreEqual(LightTransition.Off(), lastOutput);
+            
+            // Verify pass-through behavior
+            var inputTransition = new LightParameters { Brightness = 100 }.AsTransition();
+            node.Input = inputTransition;
+            Assert.AreEqual(inputTransition, lastOutput);
+        }
+
+        [TestMethod]
+        public void PassThroughOn_Triggered_PassesThrough()
+        {
+            // Arrange
+            var triggerSubject = new Subject<int>();
+            
+            // Act
+            var node = _reactiveNodeFactory.CreateReactiveNode(_lightMock.Object, config => 
+            {
+                config.PassThroughOn(triggerSubject);
+            });
+
+            LightTransition? lastOutput = null;
+            node.OnNewOutput.Subscribe(output => lastOutput = output);
+
+            // Trigger
+            triggerSubject.OnNext(1);
+
+            // Verify pass-through behavior
+            var inputTransition = new LightParameters { Brightness = 100 }.AsTransition();
+            node.Input = inputTransition;
+            Assert.AreEqual(inputTransition, lastOutput);
+        }
+
+        [TestMethod]
+        public void Input_PropagatedToActiveNode()
+        {
+            // Arrange
+            var triggerSubject = new Subject<int>();
+            
+            // Act
+            var node = _reactiveNodeFactory.CreateReactiveNode(_lightMock.Object, config => 
+            {
+                config.On(triggerSubject, context => new FactoryNode<LightTransition>(input => 
+                    input with { LightParameters = new LightParameters { Brightness = 100 } }));
+            });
+
+            LightTransition? lastOutput = null;
+            node.OnNewOutput.Subscribe(output => lastOutput = output);
+
+            triggerSubject.OnNext(1); // Activate the node
+
+            // Act
+            node.Input = new LightTransition { LightParameters = new LightParameters { Brightness = 10 } };
+
+            // Assert
+            Assert.IsNotNull(lastOutput);
+            Assert.AreEqual(100, lastOutput.LightParameters.Brightness);
         }
 
         public class ScopedPipelineNode : PipelineNode<LightTransition>
