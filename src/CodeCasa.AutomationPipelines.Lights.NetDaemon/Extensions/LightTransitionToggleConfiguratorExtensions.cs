601ec14208130937bb9bbf265135dde865d0adaa
Jasper <jasper.lammers@gmail.com>
2026-01-08 09:53:41 +0100

    Implemented NetDaemon ILightEntityCore methods for all ForLight and ForLights methods.

---
+++ src/CodeCasa.AutomationPipelines.Lights.NetDaemon/Extensions/LightTransitionToggleConfiguratorExtensions.cs
@@ -0,0 +1,41 @@
+using CodeCasa.AutomationPipelines.Lights.Toggle;
+using CodeCasa.Lights.NetDaemon.Extensions;
+using NetDaemon.HassModel.Entities;
+
+namespace CodeCasa.AutomationPipelines.Lights.NetDaemon.Extensions;
+
+/// <summary>
+/// Extension methods for light toggle configurators to work with NetDaemon light entities.
+/// </summary>
+public static class LightTransitionToggleConfiguratorExtensions
+{
+    /// <summary>
+    /// Creates a scoped toggle configuration for a specific NetDaemon light entity.
+    /// </summary>
+    /// <param name="configurator">The toggle configurator.</param>
+    /// <param name="lightEntity">The NetDaemon light entity to configure.</param>
+    /// <param name="configure">An action to configure the toggle for this specific light.</param>
+    /// <param name="excludedLightBehaviour">Specifies the behavior for lights not included in this scoped configuration. Defaults to <see cref="ExcludedLightBehaviours.None"/>.</param>
+    /// <returns>The configurator instance for method chaining.</returns>
+    public static ILightTransitionToggleConfigurator ForLight(this ILightTransitionToggleConfigurator configurator,
+        ILightEntityCore lightEntity, Action<ILightTransitionToggleConfigurator> configure,
+        ExcludedLightBehaviours excludedLightBehaviour = ExcludedLightBehaviours.None)
+    {
+        return configurator.ForLight(lightEntity.AsLight(), configure, excludedLightBehaviour);
+    }
+
+    /// <summary>
+    /// Creates a scoped toggle configuration for multiple NetDaemon light entities.
+    /// </summary>
+    /// <param name="configurator">The toggle configurator.</param>
+    /// <param name="lightEntities">The NetDaemon light entities to configure.</param>
+    /// <param name="configure">An action to configure the toggle for these lights.</param>
+    /// <param name="excludedLightBehaviour">Specifies the behavior for lights not included in this scoped configuration. Defaults to <see cref="ExcludedLightBehaviours.None"/>.</param>
+    /// <returns>The configurator instance for method chaining.</returns>
+    public static ILightTransitionToggleConfigurator ForLights(this ILightTransitionToggleConfigurator configurator,
+        IEnumerable<ILightEntityCore> lightEntities, Action<ILightTransitionToggleConfigurator> configure,
+        ExcludedLightBehaviours excludedLightBehaviour = ExcludedLightBehaviours.None)
+    {
+        return configurator.ForLights(lightEntities.Select(l => l.AsLight()), configure, excludedLightBehaviour);
+    }
+}
