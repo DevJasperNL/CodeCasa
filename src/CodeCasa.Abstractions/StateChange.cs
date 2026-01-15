564dab8b2826a897bda2bc4e7825acb03f68c0fd
Jasper Lammers <81427514+DevJasperNL@users.noreply.github.com>
2026-01-15 17:07:12 +0100

    Added StateChanges and StateChangesWithCurrent to ILight. (#192)

---
+++ src/CodeCasa.Abstractions/StateChange.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeCasa.Abstractions;
+
+/// <summary>
+/// Represents a change in state for an entity.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
+/// <typeparam name="TState">The type of the state.</typeparam>
+/// <param name="Entity">The entity whose state changed.</param>
+/// <param name="Old">The old state.</param>
+/// <param name="New">The new state.</param>
+public record StateChange<TEntity, TState>(TEntity Entity, TState? Old, TState? New);
