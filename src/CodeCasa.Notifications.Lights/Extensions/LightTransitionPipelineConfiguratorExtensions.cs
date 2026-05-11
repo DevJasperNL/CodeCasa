9c45a29f6292547d9bbadac58205c641f2673cdf
Jasper <jasper.lammers@gmail.com>
2026-05-11 16:56:38 +0200

    Updated 'AddNotifications' to resolve `LightNotificationManagerContext` from the internally provided service provider.

--- src/CodeCasa.Notifications.Lights/Extensions/LightTransitionPipelineConfiguratorExtensions.cs
+++ src/CodeCasa.Notifications.Lights/Extensions/LightTransitionPipelineConfiguratorExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using CodeCasa.AutomationPipelines.Lights.Pipeline;
 using CodeCasa.Lights;
 using System.Reactive.Linq;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeCasa.Notifications.Lights.Extensions
 {
@@ -14,17 +15,29 @@ namespace CodeCasa.Notifications.Lights.Extensions
         /// </summary>
         /// <typeparam name="TLight">The type of light being controlled.</typeparam>
         /// <param name="configurator">The pipeline configurator.</param>
-        /// <param name="lightNotificationManagerContext">The context providing light notifications.</param>
         /// <returns>The updated pipeline configurator.</returns>
         public static ILightTransitionPipelineConfigurator<TLight> AddNotifications<TLight>(
-            this ILightTransitionPipelineConfigurator<TLight> configurator, LightNotificationManagerContext lightNotificationManagerContext)
+            this ILightTransitionPipelineConfigurator<TLight> configurator)
             where TLight : ILight
         {
             return configurator.AddReactiveNode(c =>
             {
                 c
                     .SetName("Notifications")
-                    .AddNodeSource(lightNotificationManagerContext.LightNotifications.Select(lnc => lnc == null ? _ => null : lnc.CreateFactory()));
+                    .AddNodeSource(sp =>
+                    {
+                        var context = sp.GetService<LightNotificationManagerContext>();
+
+                        if (context is null)
+                        {
+                            throw new InvalidOperationException(
+                                $"Could not resolve {nameof(LightNotificationManagerContext)}. " +
+                                $"Make sure to call {nameof(ServiceCollectionExtensions.AddLightNotifications)} " +
+                                $"on your {nameof(IServiceCollection)} before using {nameof(AddNotifications)}.");
+                        }
+                        return context.LightNotifications
+                            .Select(lnc => lnc == null ? _ => null : lnc.CreateFactory());
+                    });
             });
         }
     }
