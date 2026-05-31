b5636992fea0b3d1bc2c5ea722114b61f39dbcce
Jasper <jasper.lammers@gmail.com>
2026-05-31 17:45:32 +0200

    Added addscene to LightTransitionCycleConfiguratorExtensions

--- src/CodeCasa.AutomationPipelines.Lights.NetDaemon/Extensions/LightTransitionToggleConfiguratorExtensions.cs
+++ src/CodeCasa.AutomationPipelines.Lights.NetDaemon/Extensions/LightTransitionToggleConfiguratorExtensions.cs
@@ -1,9 +1,6 @@
 using CodeCasa.AutomationPipelines.Lights.Toggle;
-using CodeCasa.Lights;
 using CodeCasa.Lights.NetDaemon;
 using CodeCasa.Lights.NetDaemon.Extensions;
-using CodeCasa.Lights.NetDaemon.Scenes;
-using Microsoft.Extensions.DependencyInjection;
 using NetDaemon.HassModel.Entities;
 
 namespace CodeCasa.AutomationPipelines.Lights.NetDaemon.Extensions;
@@ -11,7 +8,7 @@ namespace CodeCasa.AutomationPipelines.Lights.NetDaemon.Extensions;
 /// <summary>
 /// Extension methods for light toggle configurators to work with NetDaemon light entities.
 /// </summary>
-public static class LightTransitionToggleConfiguratorExtensions
+public static partial class LightTransitionToggleConfiguratorExtensions
 {
     /// <summary>
     /// Creates a scoped toggle configuration for a specific NetDaemon light entity.
@@ -43,23 +40,4 @@ public static class LightTransitionToggleConfiguratorExtensions
         return configurator.ForLights(lightEntities.Select(l => l.AsLight()), configure, excludedLightBehaviour);
     }
 
-    /// <summary>
-    /// Adds a Home Assistant scene to the toggle sequence. The scene is fetched and cached via
-    /// <see cref="LightSceneCacheService"/> on first use. If a light is not part of the scene,
-    /// no state is added for that light.
-    /// </summary>
-    /// <param name="configurator">The toggle configurator.</param>
-    /// <param name="sceneEntity">The scene entity whose light parameters will be applied.</param>
-    /// <returns>The configurator instance for method chaining.</returns>
-    public static ILightTransitionToggleConfigurator<NetDaemonLight> AddScene(this ILightTransitionToggleConfigurator<NetDaemonLight> configurator,
-        IEntityCore sceneEntity)
-    {
-        return configurator.Add(sp =>
-        {
-            var cacheService = sp.GetRequiredService<LightSceneCacheService>();
-            var sceneLights = cacheService.GetLightSceneAsync(sceneEntity.EntityId).GetAwaiter().GetResult();
-            var light = sp.GetRequiredService<ILight>();
-            return sceneLights.GetValueOrDefault(light.Id);
-        });
     }
-}
